3613fa56e61b82ca0f5652ece1034b198657134f
C <3493534+coffeemancy@users.noreply.github.com>
2023-08-24 16:00:31 -0400

    Componentize layouts and variants in order to support Beta updates (#10)
    * enforce consistent JSON formatting per python json.tool (2 space indent)
    
    Enforce consistent JSON formatting via test failing on PRs otherwise
    for non-excluded files. Use spaces only, no tabs, 2 space indent.
    Initially exclude many existing files to avoid having a massive git
    PR with changes on every line. In future, can adjust bit by bit,
    or at least for new files.
    
    * update non-excluded JSON files to match json.tool formatting
    
    * break out items/bosses/flags to components, use for map/item/broadcast
    
    Break out items, bosses, flags, and extra flags (currently empty) to
    own components files (which could be overridden on a variant basis).
    Update map tracker, items-only tracker, and broadcast view to use them.
    
    * consolidate legacy of cyrus variant layout
    
    * consolidate lost worlds layouts
    
    * consolidate vanilla rando layouts
    
    * update changelog
    
    ---------
    
    Co-authored-by: C <caltronnotlrac@gmail.com>

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,24 +2,37 @@ Tracker:AddItems("items/items.json")
 Tracker:AddMaps("maps/maps.json")
 ScriptHost:LoadScript("scripts/logic.lua")
 
-Tracker:AddLocations("locations/locations.json")
+local items_grid_component = "layouts/components/items_grid.json"
+local bosses_grid_component = "layouts/components/bosses_grid.json"
+local flags_grid_component = "layouts/components/flags_grid.json"
+local extra_flags_grid_component = "layouts/components/extra_flags_grid.json"
+local tracker_layout = "layouts/tracker.json"
+local broadcast_layout = "layouts/broadcast.json"
+
+if legacyOfCyrusMode() then
+  items_grid_component = "legacy_of_cyrus/layouts/components/items_grid.json"
+  bosses_grid_component = "legacy_of_cyrus/layouts/components/bosses_grid.json"
+elseif lostWorldsMode() then
+  items_grid_component = "lost_worlds/layouts/components/items_grid.json"
+  bosses_grid_component = "lost_worlds/layouts/components/bosses_grid.json"
+  flags_grid_component = "lost_worlds/layouts/components/flags_grid.json"
+elseif vanillaRandoMode() then
+  items_grid_component = "vanilla_rando/layouts/components/items_grid.json"
+end
 
 if string.find(Tracker.ActiveVariantUID, "items_only") then
-  Tracker:AddLayouts("items_only/layouts/tracker.json")
-  Tracker:AddLayouts("layouts/broadcast.json")
-elseif string.find(Tracker.ActiveVariantUID, "lost_world_items") then
-  Tracker:AddLayouts("lost_world_items/layouts/tracker.json")
-  Tracker:AddLayouts("lost_world_items/layouts/broadcast.json")
+  tracker_layout = "items_only/layouts/tracker.json"
 elseif lostWorldsMode() then
-  Tracker:AddLayouts("lost_worlds/layouts/tracker.json")
-  Tracker:AddLayouts("lost_worlds/layouts/broadcast.json")
-elseif vanillaRandoMode() then
-  Tracker:AddLayouts("vanilla_rando/layouts/tracker.json")
-  Tracker:AddLayouts("vanilla_rando/layouts/broadcast.json")
-else
-  Tracker:AddLayouts("layouts/tracker.json")
-  Tracker:AddLayouts("layouts/broadcast.json")
+  tracker_layout = "lost_worlds/layouts/tracker.json"
 end
+
+Tracker:AddLocations("locations/locations.json")
+Tracker:AddLayouts(items_grid_component)
+Tracker:AddLayouts(bosses_grid_component)
+Tracker:AddLayouts(flags_grid_component)
+Tracker:AddLayouts(extra_flags_grid_component)
+Tracker:AddLayouts(tracker_layout)
+Tracker:AddLayouts(broadcast_layout)
 
 if _VERSION == "Lua 5.3" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
