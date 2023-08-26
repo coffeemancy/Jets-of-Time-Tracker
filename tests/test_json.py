894750d93dcf5682b1e9ea50d71ba46e4a531923
C <caltronnotlrac@gmail.com>
2023-08-26 12:24:37 -0400

    componentize legacy of cyrus variant layout

--- tests/test_json.py
+++ tests/test_json.py
@@ -31,7 +31,6 @@ def formatted_jsonfiles(jsonfiles) -> List[Path]:
     # to avoid having large git diffs
     excluded_paths = [
         'items/',
-        'legacy_of_cyrus/',
         'locations/',
         'lost_world_items/',
         'lost_worlds/',
