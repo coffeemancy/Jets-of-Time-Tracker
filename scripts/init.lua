cece6e7a7659f923cb0d144b3c2d89423eb3119c
C <caltronnotlrac@gmail.com>
2023-08-27 17:15:28 -0400

    break out items/bosses/flags to components; settings popup
    Break out items, bosses, flags, and extra flags (currently empty) to
    own components files (which can have variant-specific overrides).
    Update map tracker, items-only tracker, and broadcast view to use them.
    Move flags to settings popup, which can work with map and items-only
    trackers.

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,37 +1,60 @@
-Tracker:AddItems("items/items.json")
-Tracker:AddMaps("maps/maps.json")
 ScriptHost:LoadScript("scripts/logic.lua")
 
-local items_grid_component = "layouts/components/items_grid.json"
-local bosses_grid_component = "layouts/components/bosses_grid.json"
-local flags_grid_component = "layouts/components/flags_grid.json"
-local extra_flags_grid_component = "layouts/components/extra_flags_grid.json"
-local tracker_layout = "layouts/tracker.json"
-local broadcast_layout = "layouts/broadcast.json"
+--
+-- Adds components used by tracker for items, bosses, flags, etc.
+--
+function addComponents()
 
-if string.find(Tracker.ActiveVariantUID, "items_only") then
-  tracker_layout = "items_only/layouts/tracker.json"
-elseif string.find(Tracker.ActiveVariantUID, "lost_world_items") then
-  tracker_layout = "lost_world_items/layouts/tracker.json"
-  broadcast_layout = "lost_world_items/layouts/broadcast.json"
-elseif lostWorldsMode() then
-  tracker_layout = "lost_worlds/layouts/tracker.json"
-  brodcast_layout = "lost_worlds/layouts/broadcast.json"
-elseif vanillaRandoMode() then
-  tracker_layout = "vanilla_rando/layouts/tracker.json"
-  broadcast_layout = "vanilla_rando/layouts/broadcast.json"
+  -- relative component paths
+  -- NOTE: PopTracker will prefix components automatically with variant name
+  -- and prefer those, but if they don't exist, will fall back to
+  -- using paths relative to pack root directory
+  local components = {}
+  components["items_grid"] = "layouts/components/items_grid.json"
+  components["bosses_grid"] = "layouts/components/bosses_grid.json"
+  components["flags_grid"] = "layouts/components/flags_grid.json"
+  components["extra_flags_grid"] = "layouts/components/extra_flags_grid.json"
+
+  for _, v in pairs(components) do Tracker:AddLayouts(v) end
+
 end
 
+--
+-- Adds main tracker and broadcast layouts based on mode and variant.
+--
+function addTrackerLayouts()
+
+  local layouts = {}
+  layouts["tracker"] = "layouts/tracker.json"
+  layouts["broadcast"] = "layouts/broadcast.json"
+  layouts["settings_popup"] = "layouts/settings_popup.json"
+
+  if itemsOnlyTracking() then
+    layouts["tracker"] = "items_only/layouts/tracker.json"
+  elseif string.find(Tracker.ActiveVariantUID, "lost_world_items") then
+    layouts["tracker"] = "lost_world_items/layouts/tracker.json"
+    layouts["broadcast"] = "lost_world_items/layouts/broadcast.json"
+  elseif lostWorldsMode() then
+    layouts["tracker"] = "lost_worlds/layouts/tracker.json"
+    layouts["broadcast"] = "lost_worlds/layouts/broadcast.json"
+  elseif vanillaRandoMode() then
+    layouts["tracker"] = "vanilla_rando/layouts/tracker.json"
+    layouts["broadcast"] = "vanilla_rando/layouts/broadcast.json"
+  end
+
+  for _, v in pairs(layouts) do Tracker:AddLayouts(v) end
+
+end
+
+-- Configure tracker
+Tracker:AddItems("items/items.json")
+Tracker:AddMaps("maps/maps.json")
 Tracker:AddLocations("locations/locations.json")
-Tracker:AddLayouts(items_grid_component)
-Tracker:AddLayouts(bosses_grid_component)
-Tracker:AddLayouts(flags_grid_component)
-Tracker:AddLayouts(extra_flags_grid_component)
-Tracker:AddLayouts(tracker_layout)
-Tracker:AddLayouts(broadcast_layout)
+addComponents()
+addTrackerLayouts()
 
 if _VERSION == "Lua 5.3" then
-    ScriptHost:LoadScript("scripts/autotracking.lua")
+  ScriptHost:LoadScript("scripts/autotracking.lua")
 else
     print("Auto-tracker is unsupported by your tracker version")
 end
