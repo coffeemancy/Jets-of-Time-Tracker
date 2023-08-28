5aa311d3380e53c116e36a81da0b6bd17cf2a2be
C <caltronnotlrac@gmail.com>
2023-08-28 17:43:05 -0400

    fix tracking Epoch fixed for Epoch Fail
    With or without Unlocked Skyways, should use updateBoss because
    using a hosted_item of fixedepoch. Just different map location
    which hosts it.

--- scripts/autotracking.lua
+++ scripts/autotracking.lua
@@ -722,11 +722,7 @@ function updateEventsAndBosses(segment)
   -- Check if the Epoch is capable of flight.
   -- This is used in the Epoch Fail mode or Vanilla Rando
   if hasFlagEnabled("EpochFail") then
-    if hasFlagEnabled("UnlockedSkyways") then
-      updateEvent("@Blackbird/Attach Epoch Wings", segment, 0x7F00D2, 0x2)
-    else
-      updateEvent("@Snail Stop/Attach Epoch Wings", segment, 0x7F00BA, 0x80)
-    end
+    updateBoss("fixedepoch", segment, 0x7F00BA, 0x80)
   end
 
 end
